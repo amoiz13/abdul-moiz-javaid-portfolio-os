e5eea4f3d8c87eb5718651c0692384a92284b735
Abdul Moiz Javaid <amoiz8067@gmail.com>
2025-12-06 00:09:54 +0500

    tweaking 3

--- apps.config.js
+++ apps.config.js
@@ -5,7 +5,7 @@ import { displaySettings } from './components/apps/settings';
 import { displayChrome } from './components/apps/chrome';
 import { displayTrash } from './components/apps/trash';
 import { displayGedit } from './components/apps/gedit';
-import { displayAboutVivek } from './components/apps/vivek';
+import { displayAboutVivek } from './components/apps/aboutMoiz';
 import { displayTerminalCalc } from './components/apps/calc';
 
 const apps = [
--- components/apps/vscode.js
+++ components/apps/vscode.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
 export default function VsCode() {
-    return (
-        <iframe src="https://github1s.com/amoiz13/amoiz13.github.io/blob/HEAD/components/ubuntu.js" frameBorder="0" title="VsCode" className="h-full w-full bg-ub-cool-grey"></iframe>
-        // this is not my work, but it's amazing!
-        // Here is the link to the original repo: https://github.com/conwnet/github1s
-    )
+  return (
+    <iframe
+      src="https://github1s.com/amoiz13/abdul-moiz-javaid-portfolio-os/blob/main/components/apps/vivek.js"
+      frameBorder="0"
+      title="VsCode"
+      className="h-full w-full bg-ub-cool-grey"
+    />
+    // this is not my work, but it's amazing!
+    // Original repo: https://github.com/conwnet/github1s
+  )
 }
 
 export const displayVsCode = () => {
-    <VsCode> </VsCode>
-}
+  return <VsCode />;
+};
